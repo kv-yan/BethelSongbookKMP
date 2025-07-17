aff8191d913f1d9fee63ec9f0ea3648a2618d66b
a <vardanyankaren924@gmail.com>
2025-07-17 07:47:21 +0400

    Bug fix , IOS share song functionality

--- composeApp/src/iosMain/kotlin/am/bethel/application/share/ShareHelperIos.kt
+++ composeApp/src/iosMain/kotlin/am/bethel/application/share/ShareHelperIos.kt
@@ -3,17 +3,25 @@ package am.bethel.application.share
 
 import am.bethel.application.share.domain.repository.ShareHelper
 import platform.UIKit.*
+import platform.UIKit.UIApplication
+import platform.UIKit.UIActivityViewController
+import platform.darwin.*
 
 class ShareHelperIos : ShareHelper {
     override fun shareText(text: String) {
-        val activityItems = listOf(text)
-        val activityViewController = UIActivityViewController(activityItems, null)
+        dispatch_async(dispatch_get_main_queue()) {
+            try {
+                val activityVC = UIActivityViewController(
+                    activityItems = listOf(text),
+                    applicationActivities = null
+                )
 
-        val application = UIApplication.sharedApplication
-        application.keyWindow?.rootViewController?.presentViewController(
-            activityViewController,
-            animated = true,
-            completion = null
-        )
+                val keyWindow = UIApplication.sharedApplication.windows.first() as? UIWindow
+                val rootVC = keyWindow?.rootViewController
+                rootVC?.presentViewController(activityVC, animated = true, completion = null)
+            } catch (e: Exception) {
+                println("iOS share error: ${e.message}")
+            }
+        }
     }
 }
--- composeApp/src/iosMain/kotlin/am/bethel/application/share/di/ShareIosModule.kt
+++ composeApp/src/iosMain/kotlin/am/bethel/application/share/di/ShareIosModule.kt
@@ -5,5 +5,8 @@ import am.bethel.application.share.ShareHelperIos
 import org.koin.dsl.module
 
 val shareIosModule = module {
-    single<ShareHelper> { ShareHelperIos() }
+    single<ShareHelper> {
+        println("helper instance created")
+        ShareHelperIos()
+    }
 }
